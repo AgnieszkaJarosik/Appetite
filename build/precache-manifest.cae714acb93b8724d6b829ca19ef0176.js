5f545e12300a20a59c65c6f7ad2eea2f7fbd176d
Agnieszka Jarosik <agajarosik@o2.pl>
2019-10-29 17:59:46 +0100

    Package updated

---
+++ build/precache-manifest.cae714acb93b8724d6b829ca19ef0176.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c37058a17067f20753ef6dece9318d",
+    "url": "/Appetite/index.html"
+  },
+  {
+    "revision": "05982804f861658d8f94",
+    "url": "/Appetite/static/css/main.5821a617.chunk.css"
+  },
+  {
+    "revision": "21c178a277b6f64868c8",
+    "url": "/Appetite/static/js/2.ce5e4dec.chunk.js"
+  },
+  {
+    "revision": "05982804f861658d8f94",
+    "url": "/Appetite/static/js/main.00a1289e.chunk.js"
+  },
+  {
+    "revision": "5a00ee3c5e8c619664ee",
+    "url": "/Appetite/static/js/runtime~main.bca9fa71.js"
+  },
+  {
+    "revision": "3aebbbc88bb58e0df9a76d95318e3d3e",
+    "url": "/Appetite/static/media/background.3aebbbc8.jpg"
+  },
+  {
+    "revision": "952cb93e5881707dd59413e7d38ee74e",
+    "url": "/Appetite/static/media/background.952cb93e.png"
+  },
+  {
+    "revision": "281ecce390431200f1fda8c828a17175",
+    "url": "/Appetite/static/media/fontello.281ecce3.eot"
+  },
+  {
+    "revision": "4a56b4bbab02f82d63e0e91aca520007",
+    "url": "/Appetite/static/media/fontello.4a56b4bb.woff2"
+  },
+  {
+    "revision": "4fa790e8d02250b0e6fbfd8dc74dd45b",
+    "url": "/Appetite/static/media/fontello.4fa790e8.svg"
+  },
+  {
+    "revision": "84e16b04bfcb3b31ee36f3439650a7f7",
+    "url": "/Appetite/static/media/fontello.84e16b04.ttf"
+  },
+  {
+    "revision": "ecf925dbb046efde8e35ef90e8370d45",
+    "url": "/Appetite/static/media/fontello.ecf925db.woff"
+  }
+]);
